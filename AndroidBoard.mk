c395b7e84e26b322c1a2cde6aaaae539c1d90e09
Packmaan7144 <davidadamdofus@gmail.com>
2016-02-06 16:24:05 +0100

    Delete AndroidBoard.mk

--- AndroidBoard.mk
+++
@@ -1,8 +0,0 @@
-# At this stage main makefiles, including product makefiles,
-# have been read, so all major variables should be available.
-
-LOCAL_PATH := $(call my-dir)
-
-include device/nvidia/s8515/kernel.mk
-
-
